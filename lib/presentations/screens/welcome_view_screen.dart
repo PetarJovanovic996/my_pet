41154aad243995ba52662643d807a5485e9cea56
PetarJovanovic996 <jovanovicp996@gmail.com>
2025-06-12 22:05:06 +0200

    preparing welcome screen

--- lib/presentations/screens/welcome_view_screen.dart
+++ lib/presentations/screens/welcome_view_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class WelcomeViewScreen extends StatelessWidget {
   const WelcomeViewScreen({super.key});
@@ -6,7 +7,48 @@ class WelcomeViewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Image.asset('assets/images/hello.jpg')),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset('assets/images/hello.jpg', fit: BoxFit.cover),
+          ),
+          SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    AppLocalizations.of(context)!.hello,
+                    style: TextStyle(
+                      fontSize: 32,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  Text(
+                    AppLocalizations.of(context)!.welcome,
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      AppLocalizations.of(context)!.welcomeButton,
+                      style: TextStyle(fontSize: 32, color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
