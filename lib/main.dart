bd3e3b6b73fa5f741bc56016595cba69fbdee3a4
PetarJovanovic996 <jovanovicp996@gmail.com>
2025-06-13 20:10:24 +0200

    Firebase configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -6,9 +7,27 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:my_pet/core/routes.dart';
 import 'package:my_pet/core/theme.dart';
 import 'package:my_pet/data/models/language.dart';
+import 'package:my_pet/firebase_options.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/util/legacy_to_async_migration_util.dart';
 
 Future<void> main() async {
   Bloc.observer = AppBlocObserver();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  const SharedPreferencesOptions sharedPreferencesOptions =
+      SharedPreferencesOptions();
+  final sharedPreferences = await SharedPreferences.getInstance();
+  await migrateLegacySharedPreferencesToSharedPreferencesAsyncIfNecessary(
+    legacySharedPreferencesInstance: sharedPreferences,
+    sharedPreferencesAsyncOptions: sharedPreferencesOptions,
+    migrationCompletedKey: 'migrationCompleted',
+  );
+
+  //TODO: shared preference iskopiran iz proslog koda!
+  //provjera i pojasnjenje
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
 }
