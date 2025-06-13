9da3248c93a24bbff62b1a9acac5e29c88a4a591
PetarJovanovic996 <jovanovicp996@gmail.com>
2025-06-13 19:08:51 +0200

    UI for WelcomeView completed

--- lib/presentations/screens/welcome_view_screen.dart
+++ lib/presentations/screens/welcome_view_screen.dart
@@ -12,37 +12,60 @@ class WelcomeViewScreen extends StatelessWidget {
           Positioned.fill(
             child: Image.asset('assets/images/hello.jpg', fit: BoxFit.cover),
           ),
-          SafeArea(
-            child: Center(
+
+          Align(
+            alignment: Alignment(0, -0.6),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  AppLocalizations.of(context)!.appName,
+                  style: TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 35, 46, 37),
+                    letterSpacing: 6,
+                  ),
+                ),
+                SizedBox(height: 16),
+              ],
+            ),
+          ),
+
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 18.0),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisSize: MainAxisSize.min,
                 children: [
                   Text(
-                    AppLocalizations.of(context)!.hello,
-                    style: TextStyle(
-                      fontSize: 32,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
+                    AppLocalizations.of(context)!.welcomeViewMessage,
+                    style: TextStyle(fontSize: 28, color: Colors.white),
                   ),
-                  SizedBox(height: 12),
+                  SizedBox(height: 8),
                   Text(
                     AppLocalizations.of(context)!.welcome,
-                    style: TextStyle(
-                      fontSize: 40,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
+                    style: TextStyle(fontSize: 24, color: Colors.white70),
                   ),
-                  SizedBox(height: 12),
+                  SizedBox(height: 16),
                   ElevatedButton(
                     onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 32,
+                        vertical: 12,
+                      ),
+                    ),
                     child: Text(
                       AppLocalizations.of(context)!.welcomeButton,
-                      style: TextStyle(fontSize: 32, color: Colors.black),
+                      style: TextStyle(fontSize: 20, color: Colors.black),
                     ),
                   ),
-                  SizedBox(height: 12),
+                  SizedBox(height: 16),
+
+                  Image.asset('assets/images/logo.jpg', height: 80),
                 ],
               ),
             ),
